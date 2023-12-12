9aca4832bc8d3cf512c7d7e12b1aa94182586059
jihwan2843 <jihwan2843@gmail.com>
2023-12-12 14:52:56 +0900

    MetaMask

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,7 +1,24 @@
-import { FC } from "react";
+import { useSDK } from "@metamask/sdk-react";
+import { Dispatch, FC, SetStateAction } from "react";
 import { Link } from "react-router-dom";
 
-const Header: FC = () => {
+interface HeaderProps {
+  account: string;
+  setAccount: Dispatch<SetStateAction<string>>;
+}
+
+const Header: FC<HeaderProps> = ({ account, setAccount }) => {
+  const { sdk } = useSDK();
+
+  const onClickMetaMask = async () => {
+    try {
+      const accounts: any = await sdk?.connect();
+
+      setAccount(accounts[0] as string);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <header className="bg-blue-100 p-2 flex justify-between">
       <div className="flex gap-4">
@@ -10,7 +27,17 @@ const Header: FC = () => {
         <Link to="/sale">Sale</Link>
       </div>
       <div>
-        <button>MetaMask Login</button>
+        {account ? (
+          <div>
+            <span>
+              {account.substring(0, 7)}...
+              {account.substring(account.length - 5)}
+            </span>
+            <button onClick={() => setAccount("")}>Logout</button>
+          </div>
+        ) : (
+          <button onClick={onClickMetaMask}>MetaMask Login</button>
+        )}
       </div>
     </header>
   );
