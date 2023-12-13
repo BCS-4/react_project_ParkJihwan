d39feab8c805e8a58b19147aea5925729d6e0000
jihwan2843 <jihwan2843@gmail.com>
2023-12-13 14:43:15 +0900

    Get My NFTs

--- frontend/src/components/MintModal.tsx
+++ frontend/src/components/MintModal.tsx
@@ -1,14 +1,21 @@
 import { Dispatch, FC, SetStateAction, useState } from "react";
 import { useOutletContext } from "react-router-dom";
-import { OutletContext, NftMetadata } from "../types/index";
 import axios from "axios";
+import { NftMetadata, OutletContext } from "../types";
 
 interface MintModalProps {
   setIsOpen: Dispatch<SetStateAction<boolean>>;
+  metadataArray: NftMetadata[];
+  setMetadataArray: Dispatch<SetStateAction<NftMetadata[]>>;
 }
 
-const MintModal: FC<MintModalProps> = ({ setIsOpen }) => {
+const MintModal: FC<MintModalProps> = ({
+  setIsOpen,
+  metadataArray,
+  setMetadataArray,
+}) => {
   const [metadata, setMetadata] = useState<NftMetadata>();
+  const [isLoading, setIsLoading] = useState<boolean>();
 
   const { mintNftContract, account } = useOutletContext<OutletContext>();
 
@@ -16,7 +23,10 @@ const MintModal: FC<MintModalProps> = ({ setIsOpen }) => {
     try {
       if (!mintNftContract || !account) return;
 
+      setIsLoading(true);
+
       await mintNftContract.methods.mintNFT().send({ from: account });
+
       // @ts-expect-error
       const balance = await mintNftContract.methods.balanceOf(account).call();
 
@@ -33,15 +43,19 @@ const MintModal: FC<MintModalProps> = ({ setIsOpen }) => {
       const response = await axios.get(metadataURI);
 
       setMetadata(response.data);
+      setMetadataArray([response.data, ...metadataArray]);
+      setIsLoading(true);
     } catch (error) {
       console.error(error);
+      setIsLoading(false);
     }
   };
+
   return (
-    <div className="fixed top-0 left-0 w-full h-full bg-white bg-opacity-70 flex justify-center items-center">
-      <div className="bg-white p-8 rounded-xl">
-        <div className="bg-pink-100 text-right mb-8">
-          <button onClick={() => setIsOpen(false)}>X</button>
+    <div className="fixed top-0 left-0 w-full h-full bg-white bg-opacity-50 flex justify-center items-center">
+      <div className="p-8 bg-white rounded-xl">
+        <div className="text-right mb-8">
+          <button onClick={() => setIsOpen(false)}>x</button>
         </div>
         {metadata ? (
           <div className="w-60">
@@ -56,19 +70,30 @@ const MintModal: FC<MintModalProps> = ({ setIsOpen }) => {
               {metadata.attributes.map((v, i) => (
                 <li key={i}>
                   <span className="font-semibold">{v.trait_type}</span>
-                  <span>{v.value}</span>
+                  <span>: {v.value}</span>
                 </li>
               ))}
             </ul>
-            <div className="bg-blue-200 w-20 p-1 rounded-md mx-auto text-center font-bold mt-4 hover:text-gray-500">
-              <button onClick={() => setIsOpen(false)}>닫기</button>
+            <div className="text-center mt-4">
+              <button
+                className="hover:text-gray-500 bg-gray-300 p-2 rounded-lg"
+                onClick={() => setIsOpen(false)}
+              >
+                닫기
+              </button>
             </div>
           </div>
         ) : (
           <>
-            <div className="">NFT를 민팅하시겠습니까?</div>
-            <div className="text-center mt-4 bg-blue-100">
-              <button onClick={onClickMint}>확인</button>
+            {isLoading ? "Loading...." : <div>NFT를 민팅하시겠습니까?</div>}
+
+            <div className="text-center mt-4">
+              <button
+                className="hover:text-gray-500 bg-gray-300 p-2 rounded-lg"
+                onClick={onClickMint}
+              >
+                확인
+              </button>
             </div>
           </>
         )}
